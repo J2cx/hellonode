eb59dd05cea5f70eb1eec616488bf0f67bee8cb1
j2cx <zhugezheloic@hotmail.com>
2014-08-27 06:26:13 +0800

    host on working ip

--- test.js
+++ test.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Node.js\n');
-}).listen(8080, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:8080/');
+}).listen(8080, "192.168.1.107");
+console.log('Server running at http://192.168.1.107:8080/');
