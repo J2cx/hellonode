ee2cd27af6238647cdcd11f1535f02a85201afea
j2cx <zhugezheloic@hotmail.com>
2014-09-10 18:51:52 +0800

    hello mongoose

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var express		= require('express');
-var app			= express();
 var bodyParser	= require('body-parser');
+var mongoose	= require('mongoose');
+var Bear		= require('./models/bear');
 
+var app	= express();
+mongoose.connect('mongodb://localhost/mydb');
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
@@ -9,11 +12,26 @@ app.use(bodyParser.json());
 var port	= process.env.PORT || 8080;
 var router	= express.Router();
 
+router.use(function(req, res, next){
+	console.log("Something is happening");
+	next();
+});
 router.get("/", function(req, res){
 	res.json({message: 'wellcome'});
 });
+router.route('/bear')
+	.post(function(req, res){
+		var bear = new Bear();
+		console.log(req.body);
+		bear.name = req.body.name;
+		bear.save(function(err){
+			if (err)
+				res.send(err);
+			res.json({ message: 'Bear Created'});
+		});
+});
 
-app.use('', router);
+app.use('/api', router);
 app.listen(port);
 console.log('Magic is happening on port ' + port);
 
