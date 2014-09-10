ba37e6f0003049b4a4b2a5ba83a3492f03eb0e37
j2cx <zhugezheloic@hotmail.com>
2014-09-10 17:43:52 +0800

    hello world body-parser and express router

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
-var express = require('express');
-var app = express();
+var express		= require('express');
+var app			= express();
+var bodyParser	= require('body-parser');
 
-app.get('', function(req, res){
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+var port	= process.env.PORT || 8080;
+var router	= express.Router();
+
+router.get("/", function(req, res){
+	res.json({message: 'wellcome'});
+});
+
+app.use('', router);
+app.listen(port);
+console.log('Magic is happening on port ' + port);
+
+/*app.get('', function(req, res){
 	res.send('hello world!');
 });
 
 var server = app.listen(3000, function(){
 	console.log('listen on port %d', server.address().port);
-});
+});*/
